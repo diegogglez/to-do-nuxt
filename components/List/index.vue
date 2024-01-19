75be50d8fd21095de0704485125b5915e6806012
diegogglez <diegomiller7@gmail.com>
2024-01-19 12:44:57 +0100

    feature: now you can add task to the lists

--- components/List/index.vue
+++ components/List/index.vue
@@ -1,7 +1,11 @@
 <script setup>
+  let taskForm = ref(false);
+  let taskName;
   const props = defineProps(['data'])
   const emit = defineEmits(['delete-list'])
   const data = props.data;
+  let tasks = ref(data.tasks.length ? data.tasks : []);
+  // console.log(data.tasks.value)
 
   const styles = `w-72 flex bg-slate-900 flex-col gap-5 border ${data.labelColor} p-5 rounded-lg`;
   const color = data.labelColor;
@@ -21,9 +25,22 @@
       method: 'PUT',
       body: {
         id: data._id,
-        newTitle: data.name
+        title: data.name,
+        tasks: tasks.value
       }
     })
+  };
+
+  const handleTaskForm = (open) => {
+    open === true ? taskForm.value = true : taskForm.value = false;
+  };
+
+  const submitTaskForm = (event) => {
+    event.preventDefault();
+    tasks.value.push(taskName);
+    updateList();
+    taskName = '';
+    handleTaskForm(false)
   };
 </script>
 
@@ -39,11 +56,29 @@
         <IconsDeleteList @click="deleteList"/>
       </button>
     </div>
-    <div class="hover:bg-slate-500 p-2 rounded">
-      <button class="flex justify-start items-center gap-5 w-full">
+    <div class="flex flex-col gap-5 p-2 rounded">
+      <button 
+        v-if="!taskForm"
+        class="flex justify-start items-center gap-5 w-full hover:bg-slate-500"
+        @click="handleTaskForm(true)">
           <IconsAdd/>
           <p>add task</p>
       </button>
+      <input
+        class="text-black font-semibold" 
+        v-for="item in tasks"
+        :value="item">
+      <form v-if="taskForm" class="w-full flex flex-col justify-center items-center p-2 gap-3">
+        <input class="w-full text-black font-semibold" type="text" v-model="taskName">
+        <div class="w-full flex justify-start items-center gap-5">
+          <button @click="submitTaskForm">
+            add task
+          </button>
+          <button @click="handleTaskForm(false)">
+            <iconsClose/>
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
--- server/api/updateList.put.ts
+++ server/api/updateList.put.ts
@@ -1,8 +1,8 @@
 import { Lists } from '../utils/db'; 
 
 export default defineEventHandler(async (event) => {
-  const { id, newTitle } = await readBody(event);
-  Lists.update(id, { name: newTitle });
+  const { id, title, tasks } = await readBody(event);
+  Lists.update(id, { name: title, tasks: tasks });
   // const list = Lists.query().equalTo('_id', id).find();
   // console.log(list);
 })
