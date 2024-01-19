a0b6f3c6db003645e3468db04bed3c7eddaac078
diegogglez <diegomiller7@gmail.com>
2024-01-19 12:44:22 +0100

    style: seed button update

--- components/Header.vue
+++ components/Header.vue
@@ -8,6 +8,6 @@
 <template>
   <div class="w-full h-32 flex flex-col justify-center items-center gap-2">
     <h1 class="font-bold text-2xl">TODO BOARD</h1>
-    <button @click="runSeed">seed</button>
+    <button @click="runSeed" class="hover:bg-slate-500 p-2 rounded">seed</button>
   </div>
 </template>
